41c967ef44cc426c5ae059a9ee16c55f04a872fc
AnyamahI <150933610+AnyamahI@users.noreply.github.com>
2024-11-30 16:31:46 +0100

    Ajout de la base de données exportée et du code source

---
+++ index.php
@@ -0,0 +1,60 @@
+
+<?php require_once __DIR__. "/templates/header.php"; ?>
+
+	<div class="container col-xxl-8 px-4 py-5">
+		<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+			<div class="col-10 col-sm-8 col-lg-6">
+				<img src="/assets/image/logo-checkit.png" class="d-block mx-lg-auto img-fluid" alt="logo_CheckIt" width="500" height="500" loading="lazy">
+			</div>
+			<div class="col-lg-6">
+				<h1 class="display-5 fw-bold lh-1 mb-3">Gardez votre liste avec vous</h1>
+				<p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+				<div class="d-grid gap-2 d-md-flex justify-content-md-start">
+				<button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+				<button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container col-xxl-8 px-4 py-5">
+		<div class="row text-center">
+			<h2>Découvrez les fonctionnalités peincipales :</h2>
+			<div class="col-md-4 my-2">
+				<div class="card w-100">
+					<div class="card-header">
+						<i class="bi bi-card-checklist"></i>
+					</div>
+					<div class="card-body">
+						<h3 class="card-title">Crée un nombre illimité de liste</h3>
+						<a href="#" class="btn btn-primary">S'inscrir</a>
+					</div>
+				</div> 
+			</div>
+
+			<div class="col-md-4 my-2">
+				<div class="card w-100">
+					<div class="card-header">
+						<i class="bi bi-tags"></i>
+					</div>
+					<div class="card-body">
+						<h3 class="card-title">Classé les listes par catégories</h3>
+						<a href="#" class="btn btn-primary">S'inscrir</a>
+					</div>
+				</div> 
+			</div>
+
+			<div class="col-md-4 my-2">
+				<div class="card w-100">
+					<div class="card-header">
+						<i class="bi bi-search"></i>
+					</div>
+					<div class="card-body">
+						<h3 class="card-title">Retrouver facilemment vos listes</h3>
+						<a href="#" class="btn btn-primary">S'inscrir</a>
+					</div>
+				</div> 
+			</div>
+		</div>
+	</div>
+
+	<?php require_once __DIR__. "/templates/footer.php" ?>
---
+++ login.php
@@ -0,0 +1,48 @@
+<?php 
+
+require_once __DIR__. "/templates/header.php"; 
+require_once __DIR__. "/lib/pdo.php"; 
+require_once __DIR__. "/lib/user.php"; 
+require_once __DIR__. "/lib/session.php"; 
+
+$errors = []; // Initialisation de la variable $errors
+
+
+if(isset($_POST['loginUser'])) {
+    $user = verifyUserLoginPassword($pdo, $_POST['email'], $_POST['password']);
+
+    if($user) {
+        $_SESSION['user'] = $user;
+        header('Location: index.php');
+    } else {
+        $errors[] = "Email ou mot de passe inccorect";
+    }
+}
+?>
+
+<div class="container col-xxl-8 px-4 py-5">
+    <h1>Se connecter</h1>
+
+    <?php
+        foreach ($errors as $errors) {?>
+            <div class="alert alert-danger" role="alert">
+                <?= $errors ?>
+            </div>
+        <?php }
+        ?>
+
+    <form action="" method="post">
+        <div class="mb-3">
+            <label for="email" classe="form-label">Email</label>
+            <input type="email" name="email" id="email" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password" classe="form-label">Mot de passe</label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>
+
+        <input type="submit" name="loginUser" value="Connexion" class="btn btn-primary>
+    </form>
+</div>
+
+<?php require_once __DIR__. "/templates/footer.php" ?>
